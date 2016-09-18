94386a806863a1f8737f9d1472aca231154c0c58
apvwinkle <apvwinkle0313@gmail.com>
2016-09-18 14:48:49 -0700

    (new) inttrapezoid
    Numerical Integration via the trapezoid rule

---
+++ inttrapezoid.m
@@ -0,0 +1,16 @@
+function sum = inttrapezoid(f, a, b, n)
+%Numerical Integration via the trapezoid rule with uniform spacing
+%   f is a function, input using a function handle; a and b are the
+%   endpoints of the interval (a < b); n is the number of partitions of the
+%   x axis from a to b (n number of subintervals)
+tic
+h = (b - a)/n;
+sum = 1/2*(f(a) + f(b));
+for i = 1:n-1
+    x = a + i*h;
+    sum = sum + f(x);
+end
+sum = sum*h;
+toc
+end
+
