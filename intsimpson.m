b6c9d8562a24b3660ee0725930ece524f1852a90
apvwinkle <apvwinkle0313@gmail.com>
2016-09-18 17:41:04 -0700

    (rev) intsimpson
    this is the new code for intsimpson

--- intsimpson.m
+++ intsimpson.m
@@ -5,8 +5,8 @@ function result = intsimpson(f,a,b,error, level, level_max)
 %   is the current level of recursion (the starting level should be 0);
 %   level_max is the maximum number of recursions allowed before the
 %   function will terminate.
+global flag %I had to declare it a global variable in both workspaces
 level = level + 1;
-disp(level);
 h = b - a;
 c = (a + b)/2;
 one_simpson = h*(f(a) + 4*f(c) + f(b))/6;
@@ -16,20 +16,18 @@ two_simpson = h*(f(a) + 4*f(d) + 2*f(c) + 4*f(e) + f(b))/12;
 
 if level >= level_max
     result = two_simpson;
+    flag = true;
     %disp('max level reached'); %if it is reached, it will display this
-    %2^max_level times
+    %a lot of times! This functionality is now included in simpson.m
 else
     if abs(two_simpson - one_simpson) < 15*error
         result = two_simpson + (two_simpson - one_simpson)/15;
-        fprintf('<error, a ='); disp(a);
-        fprintf('<error, b ='); disp(b);
     else
         left_simpson = intsimpson(f, a, c, error/2, level, level_max);
         right_simpson = intsimpson(f, c, b, error/2, level, level_max);
         result = left_simpson + right_simpson;
-        %fprintf('a ='); disp(a);
-        %fprintf('b ='); disp(b);
-        fprintf('c ='); disp(c);
+        hold on
+        line([c c], [0 f(c)]);
     end
 end
 end
