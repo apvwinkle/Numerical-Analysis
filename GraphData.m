48d414078e9879130dd386cb69403b37216c6c20
apvwinkle <apvwinkle0313@gmail.com>
2016-09-21 23:17:54 -0700

    (rev) Spectral Lines - raw data graph
    added a title and labels to the graph

--- GraphData.m
+++ GraphData.m
@@ -4,3 +4,6 @@ A = xlsread('C:\Users\April\OneDrive\Senior Year\Numerical Analysis\spectrum.xls
 f = A(:,1);
 s = A(:,2);
 plot(f,s);
+title('Astronomer''s Spectral Lines');
+xlabel('Frequency (Hz)');
+ylabel('Specific Intensity (W/Hz*m^2)');
