5ae7a4e371d98a75411cf4acc87310aad756f0a4
apvwinkle <apvwinkle0313@gmail.com>
2016-09-24 16:51:35 -0700

    (rev) Spectral Lines - raw data graph
    changed title of graph

--- GraphData.m
+++ GraphData.m
@@ -1,9 +1,10 @@
-%Graphing the Astronomer's Data
+%Graphing the Astronomer's Raw Data
+figure
 A = xlsread('C:\Users\April\OneDrive\Senior Year\Numerical Analysis\spectrum.xls');
 [r c] = size(A);
 f = A(:,1);
 s = A(:,2);
 plot(f,s);
-title('Astronomer''s Spectral Lines');
+title('Astronomer''s Raw Data');
 xlabel('Frequency (Hz)');
 ylabel('Specific Intensity (W/Hz*m^2)');
