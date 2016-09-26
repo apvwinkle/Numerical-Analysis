d80b490f49e3e5810466682aea74bb842480b06e
apvwinkle <apvwinkle0313@gmail.com>
2016-09-25 20:32:02 -0700

    (rev) trapezoid
    formatting update

--- trapezoid.m
+++ trapezoid.m
@@ -1,9 +1,10 @@
 function sum = trapezoid(f, a, b, n)
 %Numerical Integration via the trapezoid rule with uniform spacing, using
 %vectorization rather than a loop
-%   f is a function, input using a function handle; a and b are the
-%   endpoints of the interval (a < b); n is the number of partitions of the
-%   x axis from a to b (n number of subintervals)
+%   f is a function, input using a function handle;
+%   a and b are the endpoints of the interval, with a < b;
+%   n is the number of partitions of the x axis from a to b (n number of
+%        subintervals)
 tic
 h = (b - a)/n;
 x = a:h:b;
