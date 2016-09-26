6cf72eae06383e080220dbcb337cd80f653ad3eb
apvwinkle <apvwinkle0313@gmail.com>
2016-09-25 20:31:11 -0700

    (rev) inttrapezoid
    formatting update

--- inttrapezoid.m
+++ inttrapezoid.m
@@ -1,8 +1,9 @@
 function sum = inttrapezoid(f, a, b, n)
 %Numerical Integration via the trapezoid rule with uniform spacing
-%   f is a function, input using a function handle; a and b are the
-%   endpoints of the interval (a < b); n is the number of partitions of the
-%   x axis from a to b (n number of subintervals)
+%   f is a function, input using a function handle;
+%   a and b are the endpoints of the interval where a < b;
+%   n is the number of partitions of the x-axis from a to b (creating n
+%        number of subintervals).
 tic
 h = (b - a)/n;
 sum = 1/2*(f(a) + f(b));
