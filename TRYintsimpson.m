f99b4d6b932d1fb4f082bca18d84d6018a5e4da2
apvwinkle <apvwinkle0313@gmail.com>
2016-09-25 20:32:19 -0700

    (rev) TRYintsimpson
    formatting update

--- TRYintsimpson.m
+++ TRYintsimpson.m
@@ -1,10 +1,11 @@
 function result = intsimpson(f, a, b, error, level, level_max)
 %Numerical Integration via the adaptive Simpson's method
-%   f is a function, input using a function handle; a and b are the
-%   endpoints of the interval (a < b); error is the desired accuracY; level
-%   is the current level of recursion (the starting level should be 0);
+%   %   f is a function, input using a function handle;
+%   a and b are the endpoints of the interval (a < b);
+%   error is the desired accuracy;
+%   level is the current level of recursion (starting level is 0)
 %   level_max is the maximum number of recursions allowed before the
-%   function will terminate.
+%        function will terminate.
 level = level + 1;
 h = b - a;
 c = (a + b)/2;
