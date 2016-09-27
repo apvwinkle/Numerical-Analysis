26774c7ad972bf9206447bfa9045f917a53191ec
apvwinkle <apvwinkle0313@gmail.com>
2016-09-27 00:40:05 -0700

    (rev) Spectral Lines - graph lines fitted from midpoints
    cleaned up script file; took away the midpoint between the overlapping
    peaks that was showing up on the graph

--- GraphLinesFittedFromMidpointsBetweenPeaks.m
+++ GraphLinesFittedFromMidpointsBetweenPeaks.m
@@ -1,28 +1,23 @@
-%Graph Mini-Peaks, Midpoints, and Lines Connecting Midpoints
-figure
-A = xlsread('C:\Users\April\OneDrive\Senior Year\Numerical Analysis\spectrum.xls');
-f = A(:,1);
-s = A(:,2);
-plot(f,s,'Color','b','DisplayName','Raw Data');
-title('Lines Fitted from Midpoints Between Peaks');
-xlabel('Frequency (Hz)');
-ylabel('Specific Intensity (W/Hz*m^2)');
-hold on
-[y_maxs, max_indices, y_mins, min_indices] = findPeaks(s);
-scatter(f([max_indices; min_indices]), s([max_indices; min_indices]), ...
-    'Marker', 'o', 'MarkerEdgeColor','g','DisplayName','Peaks')
+%GraphLinesFittedFromMidpointsBetweenPeaks
+LoadData
 
+[y_maxs, max_indices, y_mins, min_indices] = findPeaks(s);
 e_peaks_inds = max_indices(y_maxs > 0.5000e-04);
 a_peaks_inds = min_indices(y_mins < 0.3000e-04);
 peaks_indices = sort([0; e_peaks_inds; a_peaks_inds; 4000]);
-midpoints = peaks_indices(1:end-1) + ...
+all_midpoints = peaks_indices(1:end-1) + ...
     floor((peaks_indices(2:end) - peaks_indices(1:end-1))/2);
-scatter(f(midpoints), s(midpoints), 'Marker', 'x', 'MarkerEdgeColor','k', ...
+midpoints = all_midpoints([1:4,6:7]) %remove midpoint between overlapping peaks
+
+GraphData(f,s)
+scatter(f([max_indices; min_indices]), s([max_indices; min_indices]), ...
+    'Marker', 'o', 'MarkerEdgeColor','g','DisplayName','Peaks')
+scatter(f(midpoints), s(midpoints), 'Marker', 'x', 'MarkerEdgeColor','k',...
     'DisplayName','Midpoints');
-important_midpoints = midpoints([1:4,6:7])
-plot(f(important_midpoints), s(important_midpoints), 'Color','r',...
-    'DisplayName','Lines Between Midpoints');
-legend('show', 'Location', 'southwest')
+plot(f(midpoints), s(midpoints), 'Color','r','DisplayName','Lines Between Midpoints');
+legend('show', 'Location', 'southwest');
+title('Lines Fitted from Midpoints Between Peaks');
+hold off
 
 % %Output from findPeaks.m
 % y_maxs =
