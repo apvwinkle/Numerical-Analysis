a4b2023eb6448757891bf6d1ae917a571063ef0f
apvwinkle <apvwinkle0313@gmail.com>
2016-09-27 00:40:42 -0700

    (rev) Spectral Lines - graph overlapping peaks symmetry
    cleaned up script file

--- GraphOverlappingPeaksSymmetry.m
+++ GraphOverlappingPeaksSymmetry.m
@@ -1,24 +1,17 @@
-%Graph Overlapping Peaks Symmetry
-figure
-A = xlsread('C:\Users\April\OneDrive\Senior Year\Numerical Analysis\spectrum.xls');
-f = A(:,1);
-s = A(:,2);
-plot(f(2123:2605),s(2123:2605),'Color','b','DisplayName','Raw Data');
-title('Overlapping Peaks Symmetry');
-xlabel('Frequency (Hz)');
-ylabel('Specific Intensity (W/Hz*m^2)');
-hold on
-left_bounds = 2123;
-right_bounds = 2605;
+%GraphOverlappingPeaksSymmetry
+LoadData
+
+bounds = [2123, 2605];
 inbetween = 2363;
 peaks = [2341, 2379];
+big_peak_switch = flipud(s(bounds(1):peaks(1)));
+lil_peak_switch = flipud(s(peaks(2):bounds(2)));
 
-big_peak_switch = flipud(s(2123:2341));
-lil_peak_switch = flipud(s(2379:2605));
-
-plot(f(2341:2341+ length(2123:2341)-1),big_peak_switch,'Color','g',...
-    'DisplayName','Left Peak Superimposed')
-plot(f(2379-length(2379:2605)+1:2379),lil_peak_switch,'Color','c', ...
-    'DisplayName','Right Peak Superimposed')
-legend('show','Location','southwest')
+GraphData(f(bounds(1):bounds(2)),s(bounds(1):bounds(2)))
+plot(f(peaks(1):peaks(1)+length(bounds(1):peaks(1))-1), big_peak_switch,...
+    'Color','g','DisplayName','Left Peak Superimposed')
+plot(f(peaks(2)-length(peaks(2):bounds(2))+1:peaks(2)), lil_peak_switch,...
+    'Color','c','DisplayName','Right Peak Superimposed')
+legend('show','Location','southwest');
+title('Overlapping Peaks Symmetry');
 hold off
