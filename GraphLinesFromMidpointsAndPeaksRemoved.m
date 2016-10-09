1ace44442524add6cedf4b0c49abb9c79f96efde
apvwinkle <apvwinkle0313@gmail.com>
2016-10-08 17:03:58 -0700

    (rev) Spectral Lines - graph lines from midpoints and peaks removed
    Graphing script that called FindLinesFittedFromMidpointsAndPeaksRemoved,
    now working

--- GraphLinesFromMidpointsAndPeaksRemoved.m
+++ GraphLinesFromMidpointsAndPeaksRemoved.m
@@ -1,15 +1,12 @@
 %GraphLinesFromMidpointsAndPeaksRemoved
 FindLinesFittedFromMidpointsAndPeaksRemoved
 
-%%%%NOT WORKING%%%% 
-
 GraphData(f,s)
 plot(f(1:midpoints(1)),line0E,'Color', 'r', 'DisplayName','Fitted Lines')
 plot(f(midpoints(1):midpoints(2)), line1E, 'r')
 plot(f(midpoints(2):midpoints(3)), line2E, 'r')
 plot(f(midpoints(3):midpoints(4)), line3E, 'r')
-plot(f(midpoints(4):midpoints(5)), line4E, 'r')
-plot(f(midpoints(5):midpoints(6)), line5E, 'r')
+plot(f(midpoints(4):midpoints(6)), line45E, 'r')
 plot(f(midpoints(6):midpoints(7)), line6E, 'r')
 plot(f(midpoints(7):4000),         line7E, 'r')
 legend('show', 'Location', 'southwest')
